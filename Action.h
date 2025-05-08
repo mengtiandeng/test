ce9f4122553649f1be9ef835402b3a5263d68b13
mengtiandeng <mengtiandeng90@gmail.com>
2025-05-08 14:58:02 +0800

    Create Action.h

---
+++ Action.h
@@ -0,0 +1,16 @@
+#ifndef ACTION_H
+#define ACTION_H
+#include <iostream>
+#include <pud.h>
+#include <cmd.h>
+using namespace std;
+
+classs Action
+{
+public:
+  Action();
+  ~Action();
+  
+  
+}
+#endif
